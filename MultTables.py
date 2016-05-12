de235539e59807c26fb5d75aa257c11f2aceef18
Phill Conrad <pconrad@cs.ucsb.edu>
2016-05-12 12:29:25 -0700

    working version

--- MultTables.py
+++ MultTables.py
@@ -9,4 +9,16 @@ def header_row(numCols):
     return x
 
 def table_row(rowNumber,numCols):
-    return "stub"
+    x = " " + str(rowNumber) + " |"
+    for i in range(1,numCols+1):
+        x = x + ( "%3d" % (i*rowNumber) ) + "|"
+    return x
+
+def mult_table(num_rows, num_cols):
+    x = header_row(num_cols) + "\n" + separator_row(num_cols) + "\n"
+    for i in range(1,num_rows+1):
+        x = x + table_row(i,num_cols) + "\n"
+        x = x + separator_row(num_cols) + "\n"
+    return x
+
+    
--- TestMultTables.py
+++ TestMultTables.py
@@ -21,7 +21,7 @@ import unittest
 ##---+---+---+---+
 ## 3 |  3|  6|  9|
 ##---+---+---+---+
-## 4 |  4|  8| 15| 
+## 4 |  4|  8| 12| 
 ##---+---+---+---+
 ##
 ##   makeMultTable(3,1)
@@ -57,6 +57,51 @@ class MultTableTest(unittest.TestCase):
  
     def test_header_row_3_columns(self):
         self.assertEqual(MultTables.header_row(3),"   | 1 | 2 | 3 |")
+
+    def test_table_row_row_2_with_3_columns(self):
+        expected = " 2 |  2|  4|  6|"
+        actual = MultTables.table_row(2,3)
+        self.assertEqual(actual,expected)
+ 
+    def test_table_row_row_3_with_3_columns(self):
+        expected = " 3 |  3|  6|  9|"
+        actual = MultTables.table_row(3,3)
+        self.assertEqual(actual,expected)
+
+    def test_table_row_row_4_with_3_columns(self):
+        expected = " 4 |  4|  8| 12|"
+        actual = MultTables.table_row(4,3)
+        self.assertEqual(actual,expected)
+ 
+
+    def test_mult_table_4_rows_3_columns(self):
+        expected = '''   | 1 | 2 | 3 |
+---+---+---+---+
+ 1 |  1|  2|  3|
+---+---+---+---+
+ 2 |  2|  4|  6|
+---+---+---+---+
+ 3 |  3|  6|  9|
+---+---+---+---+
+ 4 |  4|  8| 12|
+---+---+---+---+
+'''
+        actual = MultTables.mult_table(4,3)
+        self.assertEqual(actual,expected)
+
+    def test_mult_table_2_rows_2_columns(self):
+        expected = '''   | 1 | 2 |
+---+---+---+
+ 1 |  1|  2|
+---+---+---+
+ 2 |  2|  4|
+---+---+---+
+'''
+        actual = MultTables.mult_table(2,2)
+        self.assertEqual(actual,expected)
+
+        
+
 
 if __name__ == "__main__":
   unittest.main()
